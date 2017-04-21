06567c3145ec981c73952ee4523e7cedbca1da66
Florian Schroeder <schroeder.florian@gmail.com>
2017-04-21 16:25:11 +0100

    Bugfix in tdvp_1site_tree: - prepare_tens failed if node changed dimension

--- tdvp_1site_tree.m
+++ tdvp_1site_tree.m
@@ -667,13 +667,14 @@ function [Atens, CA] = prepare_Tens(Atens, para)
 	if diff(size(AS)) >= 0
 		[AS, CA] = prepare_onesiteVmat(AS.', para);			% need for transpose since m x n, m > n input needed
 	else
-		[CA, AS] = prepare_onesiteVmat(AS, para);
+		% AS: D x prod(rest)
+		[CA, AS] = prepare_onesiteVmat(AS, para);			% means that prod(rest) < D, thus AS will be smaller than before!
 		CA = CA.';
-		AS = AS.';
+		AS = AS.';											% prod(rest) x prod(rest)
+		d(mc+1) = size(AS,1);
 	end
 
 	Atens	 = tensShape(AS.', 'fold', mc+1, d);
-
 end
 
 function treeMPS = tdvp_1site_evolveTree(treeMPS,para)
